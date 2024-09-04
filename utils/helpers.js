b8411fd532660cf81b4e16c6ed3c68de9ef5c824
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-09-04 16:32:49 -0700

    finished code for auth.js and helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    format_date: (date) => {
+        console.log('Received date:', date);
+        if (!date || !(date instanceof Date)) {
+            throw new TypeError('Invalid date provided');
+        }
+        return date.toLocaleDateString();
+    },
+};
